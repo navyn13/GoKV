f4edd7ea53fd50ea54f93117fc88cff1627f91cd
Naveen <navyn13102003@gmail.com>
2026-05-27 16:18:19 +0530

    initial setup for expire ttl key- lazy cleanup approach

--- cmd/kv.go
+++ cmd/kv.go
@@ -4,26 +4,36 @@ import (
 	"os"
 	"strconv"
 	"sync"
+	"time"
 )
 
 type KV struct {
 	mu   sync.RWMutex
-	data map[string][]byte
+	data map[string]value
+}
+type value struct {
+	data   string
+	expire time.Time
 }
 
 // NewKV creates a new key-value store instance
 func NewKV() *KV {
 	return &KV{
-		data: make(map[string][]byte),
+		data: make(map[string]value),
 	}
 }
 
-func (kv *KV) Set(key, val []byte) error {
+func (kv *KV) Set(key, content []byte) error {
 	kv.mu.Lock()
 	defer kv.mu.Unlock()
-	kv.data[string(key)] = val
+	value := value{
+		data:   string(content),
+		expire: time.Time{},
+	}
+	kv.data[string(key)] = value
 	return nil
 }
+
 func (kv *KV) Delete(key []byte) error {
 	kv.mu.Lock()
 	defer kv.mu.Unlock()
@@ -35,7 +45,14 @@ func (kv *KV) Get(key []byte) ([]byte, bool) {
 	kv.mu.RLock()
 	defer kv.mu.RUnlock()
 	val, ok := kv.data[string(key)]
-	return val, ok
+	if !ok {
+		return []byte{}, false
+	}
+	if val.expire.Before(time.Now()) {
+		delete(kv.data, string(key))
+		return []byte{}, false
+	}
+	return []byte(val.data), true
 }
 
 func (kv *KV) Auth(username, password string) bool {
@@ -55,12 +72,15 @@ func (kv *KV) Incr(key []byte) (int, error) {
 	kv.mu.Lock()
 	defer kv.mu.Unlock()
 
-	val, err := strconv.Atoi(string(kv.data[string(key)]))
+	val, err := strconv.Atoi(kv.data[string(key)].data)
 	if err != nil {
 		return 0, err
 	}
 	val++
-	kv.data[string(key)] = []byte(strconv.Itoa(val))
+	kv.data[string(key)] = value{
+		data:   strconv.Itoa(val),
+		expire: time.Time{},
+	}
 	return val, nil
 }
 
@@ -68,11 +88,14 @@ func (kv *KV) Decr(key []byte) (int, error) {
 	kv.mu.Lock()
 	defer kv.mu.Unlock()
 
-	val, err := strconv.Atoi(string(kv.data[string(key)]))
+	val, err := strconv.Atoi(kv.data[string(key)].data)
 	if err != nil {
 		return 0, err
 	}
 	val--
-	kv.data[string(key)] = []byte(strconv.Itoa(val))
+	kv.data[string(key)] = value{
+		data:   strconv.Itoa(val),
+		expire: time.Time{},
+	}
 	return val, nil
 }
